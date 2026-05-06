1aaccf05ba504e507312c73dfd5591d6805c6956
Umarfarooq083 <r.umar083@gmail.com>
2026-05-06 16:17:26 +0500

    resolve issue 419

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,9 +5,6 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 
-// Avoid pinning a single CSRF token in JS. Laravel regenerates the session (and CSRF token)
-// on login/logout; relying on a stale `X-CSRF-TOKEN` header causes 419 until a full reload.
-// Let Axios read Laravel's `XSRF-TOKEN` cookie for every request instead.
 window.axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
 window.axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 window.Pusher = Pusher;
